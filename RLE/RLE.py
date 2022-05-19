e96f266c91e7a278e0f03a9c00ef38c70c2cbd74
AndreyGrabovenko <grabovenko.andrei@mail.ru>
2022-05-19 17:12:49 +0300

    Improved candy game and other files

--- RLE/RLE.py
+++ RLE/RLE.py
@@ -1,16 +1,16 @@
-
 class rle():
-    def __init__(self, compressed_file = None, file_to_compress = None):
+    def __init__(self, compressed_file=None, file_to_compress=None):
         self._compressed_file = compressed_file
         self._file_to_compress = file_to_compress
         self.__count = 1
         self.__temp = []
+
     def to_compress(self):
         self.p = len(self._file_to_compress)
-        for i in range(self.p-1):
+        for i in range(self.p - 1):
             if i <= self.p:
-                if self._file_to_compress[i] == self._file_to_compress[i+1]:
-                    self.__count +=1
+                if self._file_to_compress[i] == self._file_to_compress[i + 1]:
+                    self.__count += 1
                 else:
                     self.__temp.append(self.__count)
                     self.__temp.append(self._file_to_compress[i])
@@ -18,11 +18,12 @@ class rle():
         self.__temp.append(self.__count)
         self.__temp.append(self._file_to_compress[i])
         return self.__temp
+
     def compressed(self):
         self.__list = ''
         self._p = len(self._compressed_file)
         for i in range(0, self._p, 2):
             for j in range(self._compressed_file[i]):
-                self.__temp.append(self._compressed_file[i+1])
+                self.__temp.append(self._compressed_file[i + 1])
         self.__list = ''.join(self.__temp)
         return self.__list
--- RLE/Task042.py
+++ RLE/Task042.py
@@ -1,18 +1,18 @@
 # Реализовать RLE алгоритм. реализовать модуль сжатия и восстановления данных.
 # входные и выходные данные хранятся в отдельных текстовых файлах
-from RLE import *
+from RLE.RLE import *
 import pickle
 
-with open('file_text.txt') as f:
+with open('../file_text.txt') as f:
     file = f.read()
 wee = rle(file_to_compress=file)
 wee1 = wee.to_compress()
 y = list(wee1)
 
-with open('file_text_b.bin', 'wb') as f:
+with open('../file_text_b.bin', 'wb') as f:
     pickle.dump(y, f)
 
-with open('file_text_b.bin', 'rb') as f:
+with open('../file_text_b.bin', 'rb') as f:
     bs = pickle.load(f)
 wiz = rle(compressed_file=bs)
 wiz1 = wiz.compressed()
